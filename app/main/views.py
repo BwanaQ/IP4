91a431229480df68c021ed476edee2e5e40750e5
BwanaQ <thunjawax@gmail.com>
2021-03-16 16:46:22 +0300

    Add profile photo

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..request import get_quote
 from .forms import BlogForm, UpdateProfile
 from ..models import Blog, User
 from flask_login import login_required
-from .. import db
+from .. import db, photos
 # Views
 
 
@@ -65,4 +65,15 @@ def update_profile(uname):
 
         return redirect(url_for('.profile', uname=user.username))
 
+
+@main.route('/user/<uname>/update/pic', methods=['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
     return render_template('profile/update.html', form=form)
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tom:y6t5r4e3w2q1@localhost/hunja'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 
 class ProdConfig(Config):
