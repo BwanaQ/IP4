869b585cfa72004d94afa92afddf3a4ea60e02c5
BwanaQ <thunjawax@gmail.com>
2021-03-17 21:40:36 +0300

    styling navbar

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from flask_script import Manager, Server
 from app.models import User, Role, Blog
 from flask_migrate import Migrate, MigrateCommand
-
+from flask_moment import Moment
 
 app = create_app('development')
 
@@ -10,6 +10,7 @@ manager = Manager(app)
 manager.add_command('server', Server)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+moment = Moment(app)
 
 
 @manager.shell
