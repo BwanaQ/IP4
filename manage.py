655aad87a52155177257ad5b8126165998fa109e
BwanaQ <thunjawax@gmail.com>
2021-04-19 11:26:12 +0300

    deploy to heroku

--- manage.py
+++ manage.py
@@ -3,14 +3,17 @@ from flask_script import Manager, Server
 from app.models import User, Role, Blog, Comment
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+app = create_app('production')
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('server', Server)
 
 
-@manager.shell
+manager.shell
+
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Blog=Blog, Comment=Comment)
 
