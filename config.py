bad4f9c9aa0534d32e077eab21a07fcbf219262d
BwanaQ <thunjawax@gmail.com>
2021-04-19 11:09:28 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
